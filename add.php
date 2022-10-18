e0fc681797e5102affc49a2bf7cc460ef2980fc0
markov76 <101133846+markov76@users.noreply.github.com>
2022-10-19 01:59:09 +0300

    d

--- add.php
+++ add.php
@@ -1,6 +1,8 @@
 <?php
 header('Access-Control-Allow-Origin: http:/localhost:3000');
+header('Access-Control-Allow-headers: Accept,Content-Type','Access-Control-Allow-Header');
 header('Content-Type: application/json');
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     return 0;

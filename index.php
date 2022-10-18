b474dd100200bb66eb28ae98a3a24ad7133ab11b
markov76 <101133846+markov76@users.noreply.github.com>
2022-10-18 17:52:11 +0300

    lisätty virheenkorjaus php

--- index.php
+++ index.php
@@ -2,11 +2,17 @@
 header('Access-Control-Allow-Origin:*');
 header('Content-Type: application/json');
 
-$db = new PDO('mysql:host=localhost;dbname=todo;charcet=utf8','root', '');
-$sql="select * from task";
-$query = $db->query($sql);
-$results = $query->fetchAll(PDO::FETCH_ASSOC);
-$json = json_encode($results,JSON_PRETTY_PRINT);
-header('HTTP/1.1 200 OK');
-print $json;
-
+try {
+    $db = new PDO('mysql:host=localhost;dbname=todo;charcet=utf8','root', '');
+    $dp->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $sql="select * from task";
+    $query = $db->query($sql);
+    $results = $query->fetchAll(PDO::FETCH_ASSOC);
+    $json = json_encode($results,JSON_PRETTY_PRINT);
+    header('HTTP/1.1 200 OK');
+    print $json;
+} catch (PDOException $pdoex) {
+    header('HTTP/1.1 500 Internal Server Error');
+    $error = array('error'=>$pdoex->getMessage());
+    print json_encode($error);
+}

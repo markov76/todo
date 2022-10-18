8db8b9c317f83b573f9e61108f11d31af71aa010
markov76 <101133846+markov76@users.noreply.github.com>
2022-10-18 19:09:24 +0300

    lisätty add

--- add.php
+++ add.php
@@ -6,15 +6,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     return 0;
 }
 
+$input = json_decode(file_get_contents('php://input'));
+$description = filter_var($input->description,FILTER_UNSAFE_RAW);
+
+
 try {
     $db = new PDO('mysql:host=localhost;dbname=todo;charcet=utf8','root', '');
     $dp->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    $sql="select * from task";
-    $query = $db->query($sql);
-    $results = $query->fetchAll(PDO::FETCH_ASSOC);
-    $json = json_encode($results,JSON_PRETTY_PRINT);
+    
+    $query = $db->prepare('insert into task(description) values (:description)');
+    $query->bindValue(':description',$description,PDO::PARAM_STR);
+    $query->execute();
+
+    
     header('HTTP/1.1 200 OK');
-    print $json;
+    $data array('id' => $db->lastInsertId(),'description' => $description);
+    print json_encode($result);
 } catch (PDOException $pdoex) {
     header('HTTP/1.1 500 Internal Server Error');
     $error = array('error'=>$pdoex->getMessage());

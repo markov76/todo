3d5c5975c985f8512b94b2747921d73b6ca15a9d
markov76 <101133846+markov76@users.noreply.github.com>
2022-11-04 01:30:24 +0200

    korjaus

---
+++ update.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'inc/functions.php';
+require_once 'inc/headers.php';
+
+$input = json_decode(file_get_contents('php://input'));
+$id = filter_var($input->id,FILTER_SANITIZE_NUMBER_INT);
+$description = filter_var($input->description,FILTER_SANITIZE_NUMBER_INT);
+
+try {
+    $db = openDb();
+
+    $query = $db->prepare('update task set description=:description where id=:id');
+    $query->bindValue(':description',$description,PDO::PARAM_STR);
+    $query->bindValue(':id',$id,PDO::PARAM_INT);
+    $query->execute();
+
+    header('HTTP/1.1 200 OK');
+    $data = array('id' => $id,'description' => $description);
+    print json_encode($data);
+}   catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
